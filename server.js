dbc72791d7e3fd1fa0d24c0842e8e528894425e5
Josh Chatfield <joshchatfield@gmail.com>
2017-10-23 18:03:19 -0700

    message

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 
-// grab express
 var express = require('express');
 
-//var path = require('path');
-// create an express app
 var app = express();
-
-// create an express route for the home page
-// hompage is: http://localhost:80/
-
-
-//app.set('port',80);
 
 app.get('/', function(req, res) {
 	res.sendFile('index.html', {root: "/root/awesome-test/"});
@@ -26,28 +17,31 @@ app.get('/wikipedia',function(req,res){
 app.get('/chatfieldcreatelogo',function(req,res){
 	res.sendFile('chatfieldcreatelogo.png',{root:'/root/awesome-test/'});
 	});
+
 app.get('/chatfieldcreateicon',function(req,res){
 	res.sendFile('chatfieldcreateicon.png',{root:'/root/awesome-test/'});
 	});
+
 app.get('/favicon',function(req,res){
 	res.sendFile('favicon.ico',{root:'/root/awesome-test/'});
 	});
 
-app.get('/projects',function(req,res){
-	res.sendFile('projects.html',{root:'/root/awesome-test/'});
-	});
 app.get('/devblog',function(req,res){
 	res.sendFile('devblog.html',{root:'/root/awesome-test/'});
 	});
+
 app.get('/northwest-background',function(req,res){
 	res.sendFile('northwest-background.jpg',{root:'/root/awesome-test/'});
 	});
+
 app.get('/tribute',function(req,res){
 	res.sendFile('tribute.html',{root:'/root/awesome-test/'});
 	});
+
 app.get('/twitch',function(req,res){
 	res.sendFile('twitch.html',{root:'/root/awesome-test/'});
 	});
+
 app.get('/quote',function(req,res){
 	res.sendFile('quote.html',{root:'/root/awesome-test/'});
 	});
@@ -58,10 +52,4 @@ app.get('/quote',function(req,res){
 
 // start the server on port 80
 app.listen(80);
-
-/*
-var server = app.listen(app.get('port'),function(){
-	var port = server.address().port;
-});
-*/
 
